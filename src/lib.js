d8f1b1b7d793ac747882feb57435fd4b6c14c238
Josh Kuttler <yoshuakuttler@gmail.com>
2019-09-05 11:47:11 +0300

    create new file to export all the components

---
+++ src/lib.js
@@ -0,0 +1,27 @@
+import CircleLoader from './components/CircleLoader.vue'
+import DefaultLoader from './components/DefaultLoader.vue'
+import DualRingLoader from './components/DualRingLoader.vue'
+import EllipsisLoader from './components/EllipsisLoader.vue'
+import FacebookLoader from './components/FacebookLoader.vue'
+import GridLoader from './components/GridLoader.vue'
+import HeartLoader from './components/HeartLoader.vue'
+import HourglassLoader from './components/HourglassLoader.vue'
+import RingLoader from './components/RingLoader.vue'
+import RippleLoader from './components/RippleLoader.vue'
+import RollerLoader from './components/RollerLoader.vue'
+import SpinnerLoader from './components/SpinnerLoader.vue'
+
+export default {
+    CircleLoader,
+    DefaultLoader,
+    DualRingLoader,
+    EllipsisLoader,
+    FacebookLoader,
+    GridLoader,
+    HeartLoader,
+    HourglassLoader,
+    RingLoader,
+    RippleLoader,
+    RollerLoader,
+    SpinnerLoader,
+}
