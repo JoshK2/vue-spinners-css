3ca568447585756daba8af55e79ad52cbb952d6f
Gabriel Rodrigues <jipacoding@gmail.com>
2019-10-03 06:37:00 -0400

    feat: add size property support to heart-loader

--- src/components/HeartLoader.vue
+++ src/components/HeartLoader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-show="loading" class="lds-heart">
+    <div v-show="loading" class="lds-heart" :style="{width: `${size}px`, height: `${size}px`}">
         <div v-bind:style="[spinnerStyle]">
             <div class="div-before" v-bind:style="[spinnerStyle]"></div>
             <div class="div-after" v-bind:style="[spinnerStyle]"></div>
@@ -14,6 +14,10 @@ export default {
         loading: {
             type: Boolean,
             default: true,
+        },
+        size: {
+            type: Number,
+            default: 80,
         },
         color: {
             type: String,
@@ -34,17 +38,16 @@ export default {
 .lds-heart {
     display: inline-block;
     position: relative;
-    width: 80px;
-    height: 80px;
+
     transform: rotate(45deg);
-    transform-origin: 40px 40px;
+    transform-origin: 50% 50%;
 }
 .lds-heart > div {
-    top: 32px;
-    left: 32px;
+    top: 40%;
+    left: 40%;
     position: absolute;
-    width: 32px;
-    height: 32px;
+    width: 40%;
+    height: 40%;
     background: #fff;
     animation: lds-heart 1.2s infinite cubic-bezier(0.215, 0.61, 0.355, 1);
 }
@@ -53,16 +56,16 @@ export default {
     content: ' ';
     position: absolute;
     display: block;
-    width: 32px;
-    height: 32px;
+    width: 100%;
+    height: 100%;
     background: #fff;
 }
 .lds-heart .div-before {
-    left: -24px;
+    left: -75%;
     border-radius: 50% 0 0 50%;
 }
 .lds-heart .div-after {
-    top: -24px;
+    top: -75%;
     border-radius: 50% 50% 0 0;
 }
 @keyframes lds-heart {
