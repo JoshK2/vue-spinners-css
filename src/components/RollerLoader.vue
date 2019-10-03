4bd9f9060aea8ad962cc7594a65c2d4da079cb02
Gabriel Rodrigues <jipacoding@gmail.com>
2019-10-03 14:20:07 -0400

    feat: add size property support to roller-loader

--- src/components/RollerLoader.vue
+++ src/components/RollerLoader.vue
@@ -1,28 +1,11 @@
 <template>
-    <div v-show="loading" class="lds-roller">
-        <div>
-            <div class="div-after" v-bind:style="[spinnerStyle]"></div>
-        </div>
-        <div>
-            <div class="div-after" v-bind:style="[spinnerStyle]"></div>
-        </div>
-        <div>
-            <div class="div-after" v-bind:style="[spinnerStyle]"></div>
-        </div>
-        <div>
-            <div class="div-after" v-bind:style="[spinnerStyle]"></div>
-        </div>
-        <div>
-            <div class="div-after" v-bind:style="[spinnerStyle]"></div>
-        </div>
-        <div>
-            <div class="div-after" v-bind:style="[spinnerStyle]"></div>
-        </div>
-        <div>
-            <div class="div-after" v-bind:style="[spinnerStyle]"></div>
-        </div>
-        <div>
-            <div class="div-after" v-bind:style="[spinnerStyle]"></div>
+    <div v-show="loading" class="lds-roller" :style="{ width: `${size}px`, height: `${size}px` }">
+        <div
+            v-for="(_, index) in Array(8)"
+            :key="index"
+            :style="{ transformOrigin: `${size * 0.5}px ${size * 0.5}px` }"
+        >
+            <div class="div-after" v-bind:style="[spinnerStyle, spinnerStylePosition(index)]"></div>
         </div>
     </div>
 </template>
@@ -35,6 +18,10 @@ export default {
             type: Boolean,
             default: true,
         },
+        size: {
+            type: Number,
+            default: 80,
+        },
         color: {
             type: String,
             default: '#7f58af',
@@ -43,9 +30,31 @@ export default {
     data() {
         return {
             spinnerStyle: {
+                width: `${this.size * 0.0875}px`,
+                height: `${this.size * 0.0875}px`,
+                margin: `-${this.size * 0.05}px 0 0 -${this.size * 0.05}px`,
                 background: this.color,
             },
         }
+    },
+    methods: {
+        spinnerStylePosition(nthChild) {
+            const topLeftTable = [
+                [0.7875, 0.7875],
+                [0.7875, 0.7875],
+                [0.85, 0.7],
+                [0.8875, 0.6],
+                [0.9, 0.5],
+                [0.8875, 0.4],
+                [0.85, 0.3],
+                [0.7875, 0.2125],
+                [0.7, 0.15],
+            ]
+
+            const [top, left] = topLeftTable[nthChild].map(i => i * this.size)
+
+            return { top: `${top}px`, left: `${left}px` }
+        },
     },
 }
 </script>
@@ -54,79 +63,42 @@ export default {
 .lds-roller {
     display: inline-block;
     position: relative;
-    width: 80px;
-    height: 80px;
 }
 .lds-roller > div {
     animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-    transform-origin: 40px 40px;
 }
 .lds-roller div .div-after {
     content: ' ';
     display: block;
     position: absolute;
-    width: 7px;
-    height: 7px;
     border-radius: 50%;
     background: #fff;
-    margin: -4px 0 0 -4px;
 }
 .lds-roller div:nth-child(1) {
     animation-delay: -0.036s;
 }
-.lds-roller div:nth-child(1) .div-after {
-    top: 63px;
-    left: 63px;
-}
 .lds-roller div:nth-child(2) {
     animation-delay: -0.072s;
-}
-.lds-roller div:nth-child(2) .div-after {
-    top: 68px;
-    left: 56px;
 }
 .lds-roller div:nth-child(3) {
     animation-delay: -0.108s;
 }
-.lds-roller div:nth-child(3) .div-after {
-    top: 71px;
-    left: 48px;
-}
 .lds-roller div:nth-child(4) {
     animation-delay: -0.144s;
-}
-.lds-roller div:nth-child(4) .div-after {
-    top: 72px;
-    left: 40px;
 }
 .lds-roller div:nth-child(5) {
     animation-delay: -0.18s;
 }
-.lds-roller div:nth-child(5) .div-after {
-    top: 71px;
-    left: 32px;
-}
 .lds-roller div:nth-child(6) {
     animation-delay: -0.216s;
-}
-.lds-roller div:nth-child(6) .div-after {
-    top: 68px;
-    left: 24px;
 }
 .lds-roller div:nth-child(7) {
     animation-delay: -0.252s;
 }
-.lds-roller div:nth-child(7) .div-after {
-    top: 63px;
-    left: 17px;
-}
 .lds-roller div:nth-child(8) {
     animation-delay: -0.288s;
 }
-.lds-roller div:nth-child(8) .div-after {
-    top: 56px;
-    left: 12px;
-}
+
 @keyframes lds-roller {
     0% {
         transform: rotate(0deg);
